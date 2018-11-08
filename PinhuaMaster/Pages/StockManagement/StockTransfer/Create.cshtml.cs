748940885405a3d5f4eecc2b6482012b6dfb0042
Zhangkaiyi <116307766@qq.com>
2018-11-09 04:04:36 +0800

    UPDATE

---
+++ PinhuaMaster/Pages/StockManagement/StockTransfer/Create.cshtml.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using PinhuaMaster.Data.Entities.Pinhua;
+using PinhuaMaster.Extensions;
+using PinhuaMaster.Pages.StockManagement.StockTransfer.ViewModel;
+
+namespace PinhuaMaster.Pages.StockManagement.StockTransfer
+{
+    public class CreateModel : PageModel
+    {
+        private readonly PinhuaContext _pinhuaContext;
+        private readonly IMapper _mapper;
+
+        public CreateModel(PinhuaContext pinhuaContext, IMapper mapper)
+        {
+            _pinhuaContext = pinhuaContext;
+            _mapper = mapper;
+        }
+
+        [BindProperty]
+        public StockTransferViewModel Order { get; set; }
+        public List<SelectListItem> MovementTypeList { get; set; } = new List<SelectListItem>();
+        public List<SelectListItem> CustomerList { get; set; } = new List<SelectListItem>();
+        public List<SelectListItem> WarehouseList { get; set; } = new List<SelectListItem>();
+
+        public void OnGet()
+        {
+            MovementTypeList = BuildTypes();
+            CustomerList = _pinhuaContext.GetCustomerSelectList();
+            WarehouseList = _pinhuaContext.GetWarehouseSelectList();
+        }
+
+        public IActionResult OnGetAjaxInventory()
+        {
+            var settings = new Newtonsoft.Json.JsonSerializerSettings();
+            //EF Core中默认为驼峰样式序列化处理key
+            //settings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            //使用默认方式，不更改元数据的key的大小写
+            settings.ContractResolver = new Newtonsoft.Json.Serialization.DefaultContractResolver();
+
+            return new JsonResult(_pinhuaContext.GetInventory(), settings);
+        }
+
+        public IActionResult OnPost()
+        {
+            if (ModelState.IsValid)
+            {
+                var Rcid = _pinhuaContext.GetNewRcId();
+                var rtId = "176.1";
+                var repCase = new EsRepCase
+                {
+                    RcId = Rcid,
+                    RtId = rtId,
+                    LstFiller = 2,
+                    LstFillerName = User.Identity.Name,
+                    LstFillDate = DateTime.Now,
+                    //fillDate = DateTime.Now,
+                    //wiId = "",
+                    //state = 1,
+                };
+
+                var main = _mapper.Map<StockTransferMainDTO, StockTransferMain>(Order.Main);
+                main.ExcelServerRcid = Rcid;
+                main.ExcelServerRtid = rtId;
+                main.CustomerName = _pinhuaContext.往来单位.AsNoTracking().FirstOrDefault(p => p.单位编号 == Order.Main.CustomerId).单位名称;
+
+                var details = _mapper.Map<List<StockTransferDetailsDTO>, List<StockTransferDetails>>(Order.Details);
+                details.ForEach(i =>
+                {
+                    i.ExcelServerRcid = Rcid;
+                    i.ExcelServerRtid = rtId;
+                    i.OrderId = main.OrderId;
+                });
+
+                if (details.Count == 0)
+                {
+                    ModelState.AddModelError("", "清单不可为空");
+                    MovementTypeList = BuildTypes();
+                    CustomerList = _pinhuaContext.GetCustomerSelectList();
+                    WarehouseList = _pinhuaContext.GetWarehouseSelectList();
+                    return Page();
+                }
+                _pinhuaContext.EsRepCase.Add(repCase);
+                _pinhuaContext.StockTransferMain.Add(main);
+                _pinhuaContext.StockTransferDetails.AddRange(details);
+                _pinhuaContext.SaveChanges();
+
+                return RedirectToPage("Index");
+            }
+            else
+            {
+                MovementTypeList = BuildTypes();
+                CustomerList = _pinhuaContext.GetCustomerSelectList();
+                WarehouseList = _pinhuaContext.GetWarehouseSelectList();
+                return Page();
+            }
+        }
+
+        private List<SelectListItem> BuildTypes()
+        {
+            var types = (from p in _pinhuaContext.业务类型.AsNoTracking()
+                        where p.状态 == "Yes" && p.MvP == "GI"
+                        select p).ToList();
+            var groups = from p in types
+                         group p by p.MvP into g
+                         select g.Key;
+            var groupingTypes = new List<SelectListItem>();
+            foreach (var group in groups)
+            {
+                var optGroup = new SelectListGroup
+                {
+                    Name = group
+                };
+                foreach (var option in types)
+                {
+                    if (option.MvP == group)
+                    {
+                        groupingTypes.Add(new SelectListItem
+                        {
+                            Group = optGroup,
+                            Text = option.业务类型1 + " - " + option.类型描述,
+                            Value = option.业务类型1,
+                        });
+                    }
+                }
+            }
+            return groupingTypes;
+        }
+
+    }
+}
